2d5bd8bf7fede381172c194c2e1e86e2f9a90a4b
Daniel Hangan <hangandaniel1@gmail.com>
2024-11-13 19:52:53 +0000

    -600 px

--- components/HeroRive.tsx
+++ components/HeroRive.tsx
@@ -17,7 +17,7 @@ const HeroAnimation = () => {
 
   return (
     <div className="relative w-full h-[1400px] bg-black">
-      <div className="absolute inset-0 -top-[200px]">
+      <div className="absolute inset-0 -top-[600px]">
         <RiveComponent className="w-full h-full" />
       </div>
       <div className="absolute inset-0 bg-transparent pointer-events-auto" />
